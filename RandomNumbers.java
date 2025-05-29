7b9972b093f4b73e1a166655eb9f7c4cb02107c0
Mariya Byehan <maria_byehan@yahoo.com>
2025-05-29 22:46:52 +0200

    Java-arrays has been finished

--- RandomNumbers.java
+++ RandomNumbers.java
@@ -1,8 +1,11 @@
+import java.util.Random;
+
 public class RandomNumbers {
     public static void main(String[] args) {
         
         //Escribe un programa que genere un array de 2 dimensiones de 100 filas y 10 columnas  con números aleatorios del 0 al 99, a través de un bucle anidado (Si no te suenan estos conceptos, ¡búscalos!)
-
+        int[][] numbers = new int[100][10];
+        print2DArray(numbers);
     }
 
     /**
@@ -14,7 +17,10 @@ public class RandomNumbers {
      * 1. generate random numbers from 0 to 99
      */
 
-
+    public static int randomNumber() {
+        Random random = new Random();
+        return random.nextInt(100);
+    }
     /**
      * Function name: print2DArray
      * 
@@ -23,5 +29,13 @@ public class RandomNumbers {
      * Inside the function:
      * 1. nested loop that prints a 2D array using the randomNumber function
      */
-
+    public static void print2DArray(int[][] integers) {
+        for (int i = 0; i < integers.length; i++) {
+            for (int j = 0; j < integers[i].length; j++) {
+                integers[i][j] = randomNumber();
+                System.out.print(integers[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
